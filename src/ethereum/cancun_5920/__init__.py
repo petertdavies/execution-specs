079249e9ce140c10e24ac0202032c5b3e849232d
Peter Davies <ultratwo@gmail.com>
2024-02-19 12:42:23 -0500

    Make new fork cancun_5920

---
+++ src/ethereum/cancun_5920/__init__.py
@@ -0,0 +1,10 @@
+"""
+Ethereum cancun_5920 Hardfork
+^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+The Fifteenth  Ethereum hardfork.
+"""
+
+from ethereum.fork_criteria import Unscheduled
+
+FORK_CRITERIA = Unscheduled()
